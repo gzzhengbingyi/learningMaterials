a8edbf35dacbe373345f0ffeafb8fe4687913a4c
郑炳义 <hzzhengbingyi@corp.netease.com>
2018-01-04 15:32:21 +0800

    注释更新

--- synthesis/react-starter-kit-master/src/client.js
+++ synthesis/react-starter-kit-master/src/client.js
@@ -35,6 +35,7 @@ import router from './router';
 const context = {
   // Enables critical path CSS rendering
   // https://github.com/kriasoft/isomorphic-style-loader
+  //参数已经在app.js里面定义类型
   insertCss: (...styles) => {
     // eslint-disable-next-line no-underscore-dangle
     const removeCss = styles.map(x => x._insertCss());
@@ -43,12 +44,12 @@ const context = {
     };
   },
   // Universal HTTP client
-  fetch: createFetch(fetch, {
+  fetch: createFetch(fetch, { //返回配置好默认像的fetch函数
     baseUrl: window.App.apiUrl,
   }),
 };
 
-const container = document.getElementById('app');
+const container = document.getElementById('app'); //html.js
 let currentLocation = history.location;
 let appInstance;
 
@@ -56,7 +57,7 @@ let appInstance;
 // https://developers.google.com/web/updates/2015/09/history-api-scroll-restoration
 const scrollPositionsHistory = {};
 if (window.history && 'scrollRestoration' in window.history) {
-  window.history.scrollRestoration = 'manual';
+  window.history.scrollRestoration = 'manual'; //禁止页面刷新时候跳到之前的位置
 }
 
 // Re-render the app when window.location changes
@@ -65,7 +66,7 @@ async function onLocationChange(location, action) {
   scrollPositionsHistory[currentLocation.key] = {
     scrollX: window.pageXOffset,
     scrollY: window.pageYOffset,
-  };
+  };                  
   // Delete stored scroll position for next page if any
   if (action === 'PUSH') {
     delete scrollPositionsHistory[location.key];
@@ -93,8 +94,18 @@ async function onLocationChange(location, action) {
       return;
     }
 
+    /*这个词在react中是ssr相关的，因为ssr时服务器输出的是字符串，
+    而浏览器端需要根据这些字符串完成react的初始化工作，比如创建组件实例，这样才能响应用户操作。
+    这个过程就叫hydrate，有时候也会说re-hydrate可以把hydrate理解成给干瘪的字符串”注水”*/
+
+    /*这里就会涉及到一个「由静态 DOM Node(s) 到动态可工作 DOM Node(s)」的转变过程，可以叫做 Hydration*/
+
+    /*不像 React 15 中的 ReactDOM.render(...)，ReactDOM.hydrate(...) 总是会去尝试复用 DOM，而不再关心 checksum 是否相等3。*/
+
+
     const renderReactApp = isInitialRender ? ReactDOM.hydrate : ReactDOM.render;
     appInstance = renderReactApp(
+      //回调，它将在组件渲染或更新后执行
       <App context={context}>{route.component}</App>,
       container,
       () => {
