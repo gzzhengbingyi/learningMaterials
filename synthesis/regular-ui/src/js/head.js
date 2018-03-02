bbff1158ce60cbc3bec35e019dc3668e8be46833
郑炳义 <hzzhengbingyi@corp.netease.com>
2018-03-02 11:43:17 +0800

    UI库学习资料

---
+++ synthesis/regular-ui/src/js/head.js
@@ -0,0 +1,13 @@
+/**
+ * ------------------------------------------------------------
+ * RGUI      Regular UI库
+ * @author   sensen(rainforest92@126.com)
+ * ------------------------------------------------------------
+ */
+
+'use strict';
+
+exports.Regular = require('regularjs');
+exports.Component = require('regular-ui-base/src/component');
+exports.SourceComponent = require('regular-ui-base/src/sourceComponent');
+exports._ = require('regular-ui-base/src/_');
